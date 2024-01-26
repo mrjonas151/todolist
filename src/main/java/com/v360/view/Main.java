deeeda93d614825a56b6821ab22a7cf727730f05
Jonas <jonas.aquinos@hotmail.com>
2024-01-26 18:22:39 -0300

    First commit

--- src/main/java/com/v360/view/Main.java
+++ src/main/java/com/v360/view/Main.java
@@ -3,6 +3,6 @@ package com.v360.view;
 public class Main {
     public static void main(String[] args){
         
-            System.out.println("Hello world");
+            System.out.println("Hello world!");
     }
 }
